8601ac1924789637356c56f53a123a52a6aafa81
Gabriela Frade <gabrielafrade@msn.com>
2025-05-22 10:32:04 +0100

    added API and updated JS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayShortStory(response) {
   new Typewriter("#short-story", {
-    strings:
-      "There were only enough supplies for one of us to survive the winter. We were trapped, isolated in the wilderness at the edge of a harsh winter that would trap us here. Of course, I was the one who had planned it that way.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursos: "",
   });
 }
 
-let poemElement = document.querySelector("#poem-generator-form");
-addEventListener("submit", generatePoem);
+function generateShortStory(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "3f9ec00e23oc6a139aftba624cb575a1";
+  let prompt = `User instructions: Generate a short-story about ${instructionsInput.value}`;
+  let context =
+    "You are a short-story writer that gets it's inspiration in sci-fi and surrealism. Your mission is to generate a really short-story, maximum 5 lines. Make sure to follow user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayShortStory);
+}
+
+let shortStoryElement = document.querySelector("#short-story-generator-form");
+shortStoryElement.addEventListener("submit", generateShortStory);
